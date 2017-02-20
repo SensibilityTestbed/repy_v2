da3b07f3756d8fe6d5311467810869ea38c82de8
albert <albert.rafetseder+github@univie.ac.at>
2017-02-20 14:49:14 +0100

    Fix sensor event timestamp conversion
    It was off by a few orders of magnitude, as
    `com.snakei/SensorService.java/onSensorChanged` already converts the
    sensor event's original timestamp from nanoseconds [1] to milliseconds [2].
    
    [1] https://developer.android.com/reference/android/hardware/SensorEvent.html#timestamp
    [2] https://github.com/aaaaalbert/sensibility-testbed/blob/a0c39d2c0b2538af09d6f224d0a1768615f29d4b/app/src/main/java/com/snakei/SensorService.java#L781

--- repysensors.py
+++ repysensors.py
@@ -11,20 +11,21 @@ sensorlock = emulmisc.createlock()
 
 
 # Fetch a sensor reading, store the sensor event timestamp (whose origin
-# is the machine's boot timestamp). The constant generated lets us sync
-# sensor events with the vessel's `getruntime()`.
+# is the machine's boot timestamp) in milliseconds. The constant generated
+# lets us sync sensor events with the vessel's `getruntime()`.
 _, sensor_zulu_time, _, _, _ = sensor.get_acceleration()
-sensor_zulu_time -= emulmisc.getruntime()*1000000000
+sensor_zulu_time -= emulmisc.getruntime() * 1000
 
 
 
 def normalize_sensor_event_time(t):
-  """Convert the sensor event timestamp `t` (in nanoseconds) to
-  seconds, and rebase it so that the vessel's start time and the
+  """Convert the sensor event timestamp `t` from milliseconds (see
+  com.snakei/SensorService.java/onSensorChanged) to seconds, and
+  rebase it so that the vessel's start time and the
   sensor event time have the same origin.
   The sensor event timestamp should then align with the vessel's notion
   of "now", i.e. `getruntime()`."""
-  return (t - sensor_zulu_time)/1000000000.
+  return (t - sensor_zulu_time) / 1000.
 
 
 
@@ -32,7 +33,7 @@ def wrap3(sensor_function):
   """Generalized wrapper function for 3-axis sensor implementations that
   return a tuple containing (epochtime, eventtime, x, y, z).
   This function serializes sensor access using the sensorlock, drops
-  the systime from the return values, and normalizes the sensor event time."""
+  epochtime from the return values, and normalizes the sensor event time."""
   def wrapped_sensor_function():
     sensorlock.acquire(True)
     _, t, x, y, z = sensor_function()
