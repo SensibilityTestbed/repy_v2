9f67beea857c3f32f5a4d5f5c917136fd2a72ff9
albert <albert.rafetseder+github@univie.ac.at>
2017-02-24 14:53:47 +0100

    Fix aaaaalbert/sensibility-testbed#15
    "Sanitize `portable_popen`'s callargs"
    
    This commit ensures that all of `portable_popen`'s callargs are strings,
    and that "not too many" (an arbitrary 100 of 512 possible) arguments are
    supplied.
    
    aaaaalbert/sensibility-testbed#14 mentioned that a many-args `seash`
    command could be used to crash the app. With the fix applied in this
    commit, only the nodemanager log will contain a trace of a violation:
    ```
    OSError: Invalid argument. portable_popen's argument list is restricted to 100 entries.
    ```
    
    Also, I fixed a blanket `except` where excepting an `ImportError` only
    makes more sense (and is required now, because the updated code may
    and should raise exceptions to its caller).

--- portable_popen.py
+++ portable_popen.py
@@ -21,16 +21,38 @@ def Popen(args):
   if mobile_no_subprocess:
     raise Exception("No subprocess available on this platform!")
 
-  # On android we use a custom
-  # fork-and-run C-Python extension,
-  # because we don't have a Python binary
+  # On android we use a custom fork-and-run C-Python extension,
+  # because we don't have a Python binary.
   try:
     import android
     # Other commands (ps, wc, ifconfig, ...) don't use this extension
     # args[0] could also be empty if  no sys.executable was found
     # if len(args) >= 2 and ("python" in args[0] or not args[0]):
+
+    # We must sanitize the arguments to our JNI popen, see
+    # SensibilityTestbed/repy_v2#15:
+    #
+    # Sanitize 1, There must not be too many arguments, so as to not
+    #    overflow the JNI's 512 local reference table slots. I'll fix
+    #    the allowed maximum at an arbitrary 100 for now. This should
+    #    more than suffice for our sandbox-calling purposes.
+    POPEN_MAX_ARGS = 100
+    if len(args) > POPEN_MAX_ARGS:
+      # Do as `subprocess.Popen(["a"]*10000000)` does
+      raise OSError("Invalid argument. portable_popen's argument list " +
+          "is restricted to " + str(POPEN_MAX_ARGS) + " entries.")
+
+    # Sanitize 2, Only string args are allowed.
+    for arg in args:
+      if type(arg) is not str:
+        # Do as `subprocess.Popen([123])` does
+        raise TypeError("portable_popen's argument list must only " +
+            "contain string arguments!" )
+
+    # Args are sane, call down
     return android.popen_python(args[1:])
-  except Exception, e:
+
+  except ImportError:
     pass
 
   if os.name == 'nt':
